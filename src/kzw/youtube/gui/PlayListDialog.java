e6c9dddc18c991c7a6003c918985e7857bc19c46
me@hippo <kzw@happyw.info>
2012-11-11 18:16:04 -0500

    sort playlists

--- src/kzw/youtube/gui/PlayListDialog.java
+++ src/kzw/youtube/gui/PlayListDialog.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -105,7 +106,9 @@ class PlayListDialog extends JDialog{
             try {
                 existingList = PlayList.getList();
                 toChoose.removeAllItems();
-                for (Object o: existingList){
+                Object[] playListArray = existingList.toArray(new String[0]);
+                Arrays.sort(playListArray);
+                for (Object o: playListArray){
                     toChoose.addItem(o);
                 }
                 toChoose.setEnabled(true);
